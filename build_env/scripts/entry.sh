375c7cf46cbb0969e91563fc35983d95cfeefbbd
Kuklin István <istvan@codekuklin.com>
2020-03-25 15:31:23 +0100

    Sikeres docker build

--- build_env/scripts/entry.sh
+++ build_env/scripts/entry.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 set -x
 set -e
-cp -r /build /build_copy
-chown builder -R /build_copy
-cd /build_copy/src
-su builder -c python3 build_n_install_in_order.py order
-cd /build_copy/built_pkgs
-./collect.sh
-cp -r /build_copy /output/
+#cp -r /build /build_copy
+chown builder -R /build
+cd /build/src
+su builder -c "python3 build_n_install_in_order.py order"
+cd /build/built_pkgs
+#./collect.sh
+#rm -rf /output/*
+#cp -r /build_copy /output/
