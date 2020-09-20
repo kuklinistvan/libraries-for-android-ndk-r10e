541d0578d3032fed8c1198fa8e3264378029afb4
Kuklin István Alexander <istvan@codekuklin.com>
2020-09-20 15:27:16 +0200

    Ulfius: cmake_module_path fix

--- conan/ulfius/conanfile.py
+++ conan/ulfius/conanfile.py
@@ -1,6 +1,7 @@
 from conans import ConanFile, CMake, tools, AutoToolsBuildEnvironment
 from pykuklin.downloader import get_downloader_available_in_current_environment, Path
 from pykuklin.conan.tools import build_env_vars_set
+from pykuklin.shellutils import working_directory
 import os
 import shutil
 
@@ -21,17 +22,18 @@ class ConanFileImpl(ConanFile):
 
     def source(self):
         self.run("git clone https://github.com/babelouest/ulfius")
-        os.chdir("ulfius")
-        self.run("git checkout v2.6.5")
-        self.run("patch < ../FindPackageOverrides.cmake.patch")
+        with working_directory("ulfius"):
+            self.run("git checkout v2.6.5")
+            self.run("patch < ../0001-FindPackageOverrides.cmake.patch")
+            self.run("patch < ../0002-FixModulePath.patch")
 
     def build(self):
         sf = "ulfius"
         cmake = CMake(self)
+        cmake.definitions['CMAKE_MODULE_PATH'] = os.getcwd()
     
         with build_env_vars_set(self, libs_as_ldflags=True):
             cmake.definitions['CMAKE_IGNORE_PATH'] = '/usr/local/lib;/usr/lib;/lib'
-            cmake.definitions['CMAKE_MODULE_PATH'] = os.getcwd()
             cmake.definitions['WITH_JOURNALD'] = 'Off'
             cmake.definitions['BUILD_STATIC'] = 'On'
             cmake.configure(source_folder=sf)
