60eefc47e9ed41b386e32b42adbc9f15e4124fbf
Kuklin István Alexander <istvan@codekuklin.com>
2020-09-25 09:05:31 +0200

    Old strategy cleaned up

--- build_env/build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-set -x
-docker build -t android-ndk-r10e-libs-build-env $@
-
--- built_pkgs/collect.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-cd "$DIR"
-find ../src -iname '*.pkg.tar.xz' -exec mv '{}' . \;
-
